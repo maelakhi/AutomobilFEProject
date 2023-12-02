219df2f3a9d223a1de0f5645310392d15439d29c
E-Prasetyo <tarsisiusekoprasetyo11@gmail.com>
2023-12-02 16:19:30 +0700

    UI Login
    UI and routing

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../Layout'
+
+const Home = () => {
+    return (
+        <>
+            <Layout>
+            <div>Home</div>
+            </Layout>
+        </>
+  )
+}
+
+export default Home
