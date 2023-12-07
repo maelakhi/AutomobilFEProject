5c82af569bb2b4ccdeff83e894b50154932cb69f
E-Prasetyo <tarsisiusekoprasetyo11@gmail.com>
2023-12-07 23:42:14 +0700

    Integration API with Axois
    penyesuaian style dan intergrasi api di jadikan 1 di folder service

---
+++ src/Service/ServiceListMenu.jsx
@@ -0,0 +1,40 @@
+import axios from 'axios'
+
+const URLbase = 'http://52.237.194.35:2024'
+
+
+const ServiceListMenu = {
+
+    async GetDataCarByType(typeName) {
+        const data = await axios({
+            method: "GET",
+            url: URLbase+`/api/Type/GetTypeByName?name=${typeName}`,
+            headers:{
+                'Content-Type': 'application/json',
+                // "Authorization" : `Bearer ${token}`,
+            }
+        })
+        .then((response) => response)
+        .catch((err) => err.response)
+        
+        return data
+    },
+    
+    async GetDataCarRelateType(categoryName) {
+        const data = await axios({
+            method: "GET",
+            url: URLbase+`/api/Menu/GetMenuByTypeName?type_name=${categoryName}`,
+            headers:{
+                'Content-Type': 'application/json',
+                // "Authorization" : `Bearer ${token}`,
+            }
+        })
+        .then((response) => response)
+        .catch((err) => err.response)
+        
+        return data
+    }
+
+}
+
+export default ServiceListMenu
