a3244cdec7f14521bfd926f8c2d33b2ef2fb28f4
E-Prasetyo <tarsisiusekoprasetyo11@gmail.com>
2023-12-07 18:08:17 +0700

    add Navigasi
    Login Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from './Pages/Home'
 import LoginPage from './Pages/Login'
 import RegisterPage from './Pages/Register'
-import ResetPasswordPage from './Pages/ResetPassword'
-import CreatePasswordPage from './Pages/CreatePassword'
-import ListMenuPage from './Pages/ListMenu'
 import './App.css'
 import Layout from "./Pages/Layout";
-import ClassDetailsPage from "./Pages/ClassDetails";
 
 function App() {
 
@@ -15,13 +10,8 @@ function App() {
     <>
      <Routes>
         <Route path='/' element={<Layout />}>
-          <Route index element={<HomePage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage /> } />
-          <Route path='/resetpassword' element={<ResetPasswordPage /> } />
-          <Route path='/createpassword' element={<CreatePasswordPage /> } />
-          <Route path='/listmenu' element={<ListMenuPage /> } />
-          <Route path='/classdetails' element={<ClassDetailsPage /> } />
         </Route>
     </Routes>
     </>
