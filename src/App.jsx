c60c6b4062638fb187cc1a56971fb415b604f907
E-Prasetyo <tarsisiusekoprasetyo11@gmail.com>
2023-12-07 18:10:03 +0700

    add Navigasi
    Forgot and Create New Password

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import LoginPage from './Pages/Login'
 import RegisterPage from './Pages/Register'
+import ResetPasswordPage from './Pages/ResetPassword'
+import CreatePasswordPage from './Pages/CreatePassword'
 import './App.css'
 import Layout from "./Pages/Layout";
 
@@ -12,6 +14,8 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage /> } />
+          <Route path='/resetpassword' element={<ResetPasswordPage /> } />
+          <Route path='/createpassword' element={<CreatePasswordPage /> } />
         </Route>
     </Routes>
     </>
